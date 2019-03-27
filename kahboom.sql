39ff2ddd5e140da6237849add3f13da50ef64502
blueslime320 <marnixdikkers@gmail.com>
2019-03-27 09:04:05 +0100

    add database file

---
+++ kahboom.sql
@@ -0,0 +1,160 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 27 mrt 2019 om 08:58
+-- Serverversie: 10.1.30-MariaDB
+-- PHP-versie: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kahboom`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `antwoorden`
+--
+
+CREATE TABLE `antwoorden` (
+  `antwoord-ID` int(11) NOT NULL,
+  `vraag-ID` int(11) NOT NULL,
+  `antwoord` varchar(50) NOT NULL,
+  `goed` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `gebruiker`
+--
+
+CREATE TABLE `gebruiker` (
+  `Gebruiker-ID` int(11) NOT NULL,
+  `Naam` text NOT NULL,
+  `E-mail` text NOT NULL,
+  `Wachtwoord` varchar(20) NOT NULL,
+  `Admin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `quiz-ID` int(11) NOT NULL,
+  `Naam` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `quiz spelers`
+--
+
+CREATE TABLE `quiz spelers` (
+  `quiz spelers-ID` int(11) NOT NULL,
+  `quiz_ID` int(11) NOT NULL,
+  `gebruiker-ID` int(11) NOT NULL,
+  `tijd` datetime NOT NULL,
+  `score` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `vragen`
+--
+
+CREATE TABLE `vragen` (
+  `vraag-ID` int(11) NOT NULL,
+  `quize-ID` int(11) NOT NULL,
+  `vraag` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `antwoorden`
+--
+ALTER TABLE `antwoorden`
+  ADD PRIMARY KEY (`antwoord-ID`);
+
+--
+-- Indexen voor tabel `gebruiker`
+--
+ALTER TABLE `gebruiker`
+  ADD PRIMARY KEY (`Gebruiker-ID`);
+
+--
+-- Indexen voor tabel `quiz`
+--
+ALTER TABLE `quiz`
+  ADD PRIMARY KEY (`quiz-ID`);
+
+--
+-- Indexen voor tabel `quiz spelers`
+--
+ALTER TABLE `quiz spelers`
+  ADD PRIMARY KEY (`quiz spelers-ID`);
+
+--
+-- Indexen voor tabel `vragen`
+--
+ALTER TABLE `vragen`
+  ADD PRIMARY KEY (`vraag-ID`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `antwoorden`
+--
+ALTER TABLE `antwoorden`
+  MODIFY `antwoord-ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `gebruiker`
+--
+ALTER TABLE `gebruiker`
+  MODIFY `Gebruiker-ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `quiz`
+--
+ALTER TABLE `quiz`
+  MODIFY `quiz-ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `quiz spelers`
+--
+ALTER TABLE `quiz spelers`
+  MODIFY `quiz spelers-ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `vragen`
+--
+ALTER TABLE `vragen`
+  MODIFY `vraag-ID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
